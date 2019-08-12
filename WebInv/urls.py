0cb586dea9046ad471cf0bea1d2b667a52d8e3ca
Your Name <you@example.com>
2019-08-12 23:13:34 +0900

    Add Image

--- WebInv/urls.py
+++ WebInv/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('invapp.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
